ba98f244f3e0e4fe6e2bd89b8b2fb0ff3054ca30
Mauro Delazeri <maurodelazeri@gmail.com>
2019-07-07 14:10:08 -0400

    update

--- conanfile.py
+++ conanfile.py
@@ -6,11 +6,10 @@ from conans import ConanFile, CMake, tools
 
 class LibwebsocketsConan(ConanFile):
     name = "libwebsockets"
-    version = "3.1.1"
+    version = "3.1.0"
     description = "Canonical libwebsockets.org websocket library"
     url = "https://github.com/zinnion/conan-libwebsockets"
-    #homepage = "https://github.com/warmcat/libwebsockets"
-    homepage = "https://github.com/maurodelazeri/libwebsockets"
+    homepage = "https://github.com/warmcat/libwebsockets"
     license = "LGPL-2.1"
     exports = "LICENSE.md"
     exports_sources = "CMakeLists.txt"
