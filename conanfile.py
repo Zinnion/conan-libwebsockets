ba8d8159378383b7f00dae3054f0f1cdb9a5c4bb
Mauro Delazeri <maurodelazeri@gmail.com>
2019-11-29 07:30:51 -0500

    changes

--- conanfile.py
+++ conanfile.py
@@ -1,44 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 from conans import ConanFile, CMake, tools
 
+
 class LibwebsocketsConan(ConanFile):
     name = "libwebsockets"
-    version = "3.1.0"
+    version = "3.2.0"
     description = "Canonical libwebsockets.org websocket library"
     url = "https://github.com/zinnion/conan-libwebsockets"
     homepage = "https://github.com/warmcat/libwebsockets"
     license = "LGPL-2.1"
+    topics = ("conan", "libwebsockets", "websocket")
     exports = "LICENSE.md"
     exports_sources = "CMakeLists.txt"
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
     options = {
         "shared": [True, False],
+        "fPIC": [True, False],
         "lws_with_libuv": [True, False],
         "lws_with_libevent": [True, False],
         "lws_with_zlib": [True, False],
-        "lws_with_ssl": [True, False],
-        "lws_wit_raw": [True, False],
-        "lws_with_plugins": [True, False]
+        "lws_with_ssl": [True, False]
     }
     default_options = {
         'shared': False,
-        'lws_with_libuv': True,
-        'lws_with_libevent': True,
-        'lws_with_zlib': True,
-        'lws_with_ssl': True,
-        'lws_wit_raw': True,
-        'lws_with_plugins': False,
+        'fPIC': True,
+        'lws_with_libuv': False,
+        'lws_with_libevent': False,
+        'lws_with_zlib': False,
+        'lws_with_ssl': False
     }
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
     def configure(self):
         del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
 
     def requirements(self):
         if self.options.lws_with_libuv:
@@ -48,14 +55,15 @@ class LibwebsocketsConan(ConanFile):
         if self.options.lws_with_zlib:
             self.requires.add("zlib/1.2.11@zinnion/stable")
         if self.options.lws_with_ssl:
-            self.requires.add("OpenSSL/1.1.1b@zinnion/stable")
-
+            self.requires.add("OpenSSL/1.1.1d@zinnion/stable")
+        
     def source(self):
-        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
+        sha256 = "5e731c536a20d9c03ae611631db073f05cd77bf0906a8c30d2a13638d4c8c667"
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["LWS_WITHOUT_TESTAPPS"] = True
         cmake.definitions["LWS_LINK_TESTAPPS_DYNAMIC"] = True
@@ -65,27 +73,27 @@ class LibwebsocketsConan(ConanFile):
         cmake.definitions["LWS_WITH_LIBUV"] = self.options.lws_with_libuv
         cmake.definitions["LWS_WITH_LIBEVENT"] = self.options.lws_with_libevent
         cmake.definitions["LWS_WITH_ZLIB"] = self.options.lws_with_zlib
-        cmake.definitions["LWS_ROLE_RAW_PROXY"] = self.options.lws_wit_raw
-        cmake.definitions["LWS_WITH_PLUGINS"] = self.options.lws_with_plugins
         if not self.options.lws_with_zlib:
             cmake.definitions["LWS_WITHOUT_EXTENSIONS"] = True
             cmake.definitions["LWS_WITH_ZIP_FOPS"] = False
-        # zlib is required for extensions
+            
+        if not self.options.shared and self.settings.os != "Windows":
+            cmake.definitions["LWS_STATIC_PIC"] = self.options.fPIC
 
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
         cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-       self.copy("*.h", dst="include", src=("%s/include" % self._source_subfolder))
-       self.copy("*.so*", dst="lib/debug", keep_path=False)
-       self.copy("*.so*", dst="lib/release", keep_path=False)
-       self.copy("*.dylib*", dst="lib/debug", keep_path=False)
-       self.copy("*.dylib*", dst="lib/release", keep_path=False)
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.debug.libdirs = ["lib/debug"]
-        self.cpp_info.release.libdirs = ["lib/release"]        
-        self.cpp_info.release.libdirs = ["lib"]        
+        if self.settings.os == "Windows":
+            self.cpp_info.libs.append("ws2_32")
